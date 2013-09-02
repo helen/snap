a72c8fb6343e77101213d5785065e34e6b499d31
Drew Jaynes <drew@10up.com>
2013-09-02 17:59:04 -0600

    Flesh out docs.

--- snap.php
+++ snap.php
@@ -13,32 +13,40 @@ License URI: http://opensource.org/licenses/MIT
 class Snap_Plugin {
 	/**
 	 * Set up hooks
+	 *
+	 * @since 0.1
 	 */
 	public function __construct() {
 		add_filter( 'login_redirect', array( $this, 'login_redirect' ), 10, 3 );
-		add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
+		add_action( 'pre_get_posts',  array( $this, 'pre_get_posts' ) );
 		add_action( 'add_attachment', array( $this, 'add_attachment' ) );
 	}
 
 	/**
-	 * Kick users to upload when logging in.
-	 * @param string $redirect_to Redirect location
-	 * @param string $request Any redirect location passed via URL
-	 * @param object $user Current user
-	 * @return string Redirect location
+	 * Redirect users to media-new when logging in
+	 *
+	 * @since 0.1
+	 *
+	 * @param string  $redirect_to Redirect location.
+	 * @param string  $request     Any redirect location passed via URL.
+	 * @param WP_User $user        Current user object.
+	 *
+	 * @return string Redirect location.
 	 */
 	public function login_redirect( $redirect_to, $request, $user ) {
 		return admin_url( 'media-new.php' );
 	}
 
 	/**
-	 * Only show attachments in certain main queries.
+	 * Only show attachments in certain main queries
 	 *
-	 * @param WP_Query $query Query object
+	 * @since 0.1
+	 *
+	 * @param WP_Query $query Query object.
 	 * @return void
 	 */
 	public function pre_get_posts( $query ) {
-		// only do this for the main loop on home and archive views. for now.
+		// Only do this for the main loop on home and archive views. for now.
 		if ( ! $query->is_main_query() || ( ! $query->is_home() && ! $query->is_archive() ) )
 			return;
 
@@ -49,28 +57,33 @@ class Snap_Plugin {
 	/**
 	 * Add the action to update the 'post_date' field on upload
 	 *
+	 * @since 0.1
+	 *
 	 * @param int $post_id The attachment ID
 	 */
 	public function add_attachment( $post_id ) {
-		// only alter the publish date when first uploading
+		// Only alter the publish date when first uploading
 		add_filter( 'wp_update_attachment_metadata', array( $this, 'update_image_date' ), 10, 2 );
 	}
 
 	/**
 	 * Update the 'post_date' field with the date in the image meta
 	 *
-	 * @param array $data The attachment meta array.
-	 * @param int $post_id The attachment ID.
+	 * @since 0.1
+	 *
+	 * @param array $data    The attachment meta array.
+	 * @param int   $post_id The attachment ID.
 	 *
 	 * @return array The image meta data array.
 	 */
 	function update_image_date( $data, $post_id ) {
-		// no loops :)
-		// we don't add this back because we only want it to run once per attachment
+		// No loops :)
+		// We don't add this back because we only want it to run once per attachment
 		remove_filter( 'wp_update_attachment_metadata', array( $this, 'update_image_date' ), 10, 2 );
 
+		// If the created-date is saved in EXIF data
 		if ( isset( $data['image_meta']['created_timestamp'] ) ) {
-			// Save the original date
+			// Save the WordPress-generated publish date
 			$original = get_post_field( 'post_date', $post_id );
 			update_post_meta( $post_id, '_original_upload', $original );
 
